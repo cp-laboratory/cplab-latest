b4f118a72449cd5bf7c9f6802f2c72fd7ca30be0
hasanshahriar32 <hasanshahriar32@gmail.com>
2025-11-03 11:26:31 +0600

    refactor: comment out unused target attribute and SVG in BeforeNavLinks component

--- components/payload/BeforeNavLinks.tsx
+++ components/payload/BeforeNavLinks.tsx
@@ -9,7 +9,7 @@ export const BeforeNavLinks: React.FC = () => {
       <Link 
         href="/" 
         className="nav__link"
-        target="_blank"
+        // target="_blank"
         rel="noopener noreferrer"
         style={{
           display: 'flex',
@@ -21,7 +21,7 @@ export const BeforeNavLinks: React.FC = () => {
           borderBottom: '1px solid var(--theme-elevation-150)',
         }}
       >
-        <svg 
+        {/* <svg 
           width="20" 
           height="20" 
           viewBox="0 0 24 24" 
@@ -34,7 +34,7 @@ export const BeforeNavLinks: React.FC = () => {
           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
           <polyline points="15 3 21 3 21 9"></polyline>
           <line x1="10" y1="14" x2="21" y2="3"></line>
-        </svg>
+        </svg> */}
         <span>Home</span>
       </Link>
     </div>
